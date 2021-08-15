69fe768541ae5e6a0722867be35c9f6e0eb94140
liu <liu@atguigu.com>
2021-08-15 17:08:10 +0800

    my first commit by idea master

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hello git!!!");
         System.out.println("Hello git11111!!!");
         System.out.println("Hello git22222!!!");
+        System.out.println("master hello 1111");
 
     }
 }
